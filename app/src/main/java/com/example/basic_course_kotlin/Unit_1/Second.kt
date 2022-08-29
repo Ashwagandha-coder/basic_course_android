5b57b32906baf425ee73b52cb10723e1241cf086
Ashawagandha-coder <max.cacz@yandex.ru>
2022-08-29 11:55:51 +0500

    refactoring

--- app/src/main/java/com/example/basic_course_kotlin/Unit_1/Second.kt
+++ app/src/main/java/com/example/basic_course_kotlin/Unit_1/Second.kt
@@ -2,6 +2,9 @@ package com.example.basic_course_kotlin.Unit_1
 
 fun main() {
 
+    val age = 5
+    val name = "rover"
+
 
     println("Happy Birthday, Rover!")
 
@@ -18,6 +21,12 @@ fun main() {
 
     println("You are already 5!")
     println("5 is the very best age to celebrate!")
+
+
+    println("")
+
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
 
 
 
